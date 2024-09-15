b3caf97172f2f6eb00d2dec655a101900638f7c8
Maryia Liashko <maryia.liashko@devexperts.com>
2024-09-15 15:46:35 +0200

    Added reverse binary task solution.

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,13 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    NSMutableString *binaryRepresentation = [[NSMutableString alloc] initWithCapacity:8];
+
+    for (int i = 7; i >= 0; i--) {
+        UInt8 bit = (n >> i) & 1;
+        [binaryRepresentation insertString:[NSString stringWithFormat:@"%u", bit] atIndex:0];
+        // Decided to do this way, because we still would need to reverse the string from the normal order, but I'm not sure about the performance itself
+    }
+
+    return (UInt8)strtoul([binaryRepresentation UTF8String], NULL, 2);
 }
