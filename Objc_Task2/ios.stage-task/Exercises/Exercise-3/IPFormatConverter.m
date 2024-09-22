e3dc5038ae9b060b0711f9587ef1148c31edf0bc
Maryia Liashko <maryia.liashko@devexperts.com>
2024-09-22 15:20:29 +0200

    Added solution to format converter task.

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,30 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    if (numbersArray == nil || numbersArray.count == 0) {
+        return @"";
+    }
+
+    NSMutableArray *ipNumbers = [NSMutableArray arrayWithCapacity:4];
+
+    for (int i = 0; i <= 3; i++) {
+        if (numbersArray.count > i) {
+            int number = [numbersArray[i] intValue];
+
+            if (number < 0) {
+                return @"Negative numbers are not valid for input.";
+            }
+            if (number > 255) {
+                return @"The numbers in the input array can be in the range from 0 to 255.";
+            }
+
+            [ipNumbers addObject:@(number).stringValue];
+        } else {
+            [ipNumbers addObject:@"0"];
+        }
+    }
+
+    return [ipNumbers componentsJoinedByString:@"."];
 }
 
 @end
